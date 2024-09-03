be07c545f401862508ab6d5e62d783db6e83a6ca
Ziaul Islam Bablu <ziaulislambablu2@gmail.com>
2024-09-03 14:05:28 +0600

    Time: 264 ms (38.10%), Space: 42 MB (77.24%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,24 +1,24 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-         String str = a;
-        int aa = a.length();
-        int bb = b.length();
-        int cc = bb/aa;
-       int count = 1;
-            
-            
-            for (int i = 0;i<cc+2; i++){
-                if(a.contains(b)){
-                    return count;
-                }
-                else{
-                    a+=str;
-                    count++;
-                    
-                    
-                }
-                
-            }
+        StringBuilder s = new StringBuilder(a);
+        int count = 1;
+
+        while (s.length() < b.length()) {
+            s.append(a);
+            count++;
+        }
+
+        if (s.indexOf(b) != -1) {
+            return count;
+        }
+
+        s.append(a);
+        count++;
+
+        if (s.indexOf(b) != -1) {
+            return count;
+        }
+
         return -1;
     }
 }
