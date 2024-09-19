7befbbe38a3cb4bb4b98a8065d964fd73dfcf06a
Ziaul Islam Bablu <ziaulislambablu2@gmail.com>
2024-09-19 14:07:43 +0600

    Time: 1 ms (84.09%), Space: 41 MB (35.02%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+        int reverse = 0;
+        
+        while(x!=0){
+         int digit = x % 10;
+            
+            
+      if(reverse<Integer.MIN_VALUE/10 || reverse>Integer.MAX_VALUE/10){
+                return 0;
+            }
+            
+            
+            reverse = reverse * 10 + digit;
+            x/=10;
+            
+            
+        }
+        return reverse;
+    }
+}
