e626008daa6168a112417de652fd9bdc920a556c
Ziaul Islam Bablu <ziaulislambablu2@gmail.com>
2024-11-11 21:03:11 +0600

    Time: 0 ms (100.00%), Space: 43.2 MB (20.44%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,20 +1,21 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-         int start = 0;
-        int end = nums.length-1;
-        while (start<=end){
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            // Corrected the calculation of mid
             int mid = start + (end - start) / 2;
-            if (nums[mid]==target){
-               return mid;
-            }
-            else if(nums[mid]>target){
-                end = mid -1;
-            }
-            else{
-                start = start+1;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1; // Correctly update start
             }
         }
+
         return start;
-        
     }
 }
